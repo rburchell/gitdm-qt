61cdf1f07a1b8ef18d89a5dec09fd153f9b6be77
Robin Burchell <viroteck@viroteck.net>
2011-02-28 17:45:14 +0000

    Add emails to the CSV for easy identification of unknown contributors

--- csv.py
+++ csv.py
@@ -4,11 +4,12 @@
 import sys, datetime
 
 class CSVStat:
-    def __init__ (self, name, employer, date):
+    def __init__ (self, name, employer, date, email):
         self.name = name
         self.employer = employer
         self.added = self.removed = 0
         self.date = date
+        self.email = email
     def accumulate (self, p):
         self.added = self.added + p.added
         self.removed = self.removed + p.removed
@@ -20,7 +21,7 @@ def AccumulatePatch (p):
     authdatekey = "%s-%s"%(p.author.name, date)
     if authdatekey not in PeriodCommitHash:
         empl = p.author.emailemployer (p.email, p.date)
-        stat = CSVStat (p.author.name, empl, date)
+        stat = CSVStat (p.author.name, empl, date, p.email)
         PeriodCommitHash[authdatekey] = stat
     else:
         stat = PeriodCommitHash[authdatekey]
@@ -29,10 +30,12 @@ def AccumulatePatch (p):
 def OutputCSV (file):
     if file is None:
         return
-    file.write ("Name\tAffliation\tDate\tAdded\tRemoved\n")
+    file.write ("Name\tAffliation\tDate\tAdded\tRemoved\tEmails\n")
     for date, stat in PeriodCommitHash.items():
         # sanitise names " is common and \" sometimes too
         empl_name = stat.employer.name.replace ("\"", ".").replace ("\\", ".")
         author_name = stat.name.replace ("\"", ".").replace ("\\", ".")
-        file.write ("\"%s\"\t\"%s\"\t%s\t%d\t%d\n"%(author_name, empl_name, stat.date, \
-                                                    stat.added, stat.removed))
+        author_email = stat.email.replace ("\"", ".").replace ("\\", ".")
+        file.write ("\"%s\"\t\"%s\"\t%s\t%d\t%d\t%s\n"%(author_name, empl_name, stat.date, \
+                                                    stat.added, stat.removed,
+                                                    author_email))
